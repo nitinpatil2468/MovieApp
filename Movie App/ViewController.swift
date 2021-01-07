c6a46efba2fcc0bbecbb75271389f70326977085
nitinPatil947 <nitin.patil@baseinformation.com>
2021-01-07 16:36:24 +0530

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Nitin Patil on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
